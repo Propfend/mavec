de2e9e3d7887e98a793798c24dc509a5bf931a2b
Propfend <mellomello2030@gmail.com>
2025-01-06 13:13:48 -0300

    adding gitignore.

--- target/debug/deps/mavec-52df57aaa19c5e6f.d
+++
@@ -1,8 +0,0 @@
-C:\programação\rust\oss\mavec\target\debug\deps\libmavec-52df57aaa19c5e6f.rmeta: src\lib.rs src\core.rs src\error.rs src\result.rs
-
-C:\programação\rust\oss\mavec\target\debug\deps\mavec-52df57aaa19c5e6f.d: src\lib.rs src\core.rs src\error.rs src\result.rs
-
-src\lib.rs:
-src\core.rs:
-src\error.rs:
-src\result.rs:
