0d196e66112210292461eb340b3484ac0b4273e1
Propfend <mellomello2030@gmail.com>
2025-01-06 13:05:57 -0300

    changing lib rust edition to 2021

---
+++ target/debug/deps/itoa-775b8679d7723b3d.d
@@ -0,0 +1,6 @@
+C:\programação\rust\oss\mavec\target\debug\deps\libitoa-775b8679d7723b3d.rmeta: C:\Users\PorraGames\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.14\src\lib.rs C:\Users\PorraGames\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.14\src\udiv128.rs
+
+C:\programação\rust\oss\mavec\target\debug\deps\itoa-775b8679d7723b3d.d: C:\Users\PorraGames\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.14\src\lib.rs C:\Users\PorraGames\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.14\src\udiv128.rs
+
+C:\Users\PorraGames\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.14\src\lib.rs:
+C:\Users\PorraGames\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\itoa-1.0.14\src\udiv128.rs:
---
+++ target/debug/deps/mavec-a8c6ac9d853d3569.d
@@ -0,0 +1,8 @@
+C:\programação\rust\oss\mavec\target\debug\deps\libmavec-a8c6ac9d853d3569.rmeta: src\lib.rs src\core.rs src\error.rs src\result.rs
+
+C:\programação\rust\oss\mavec\target\debug\deps\mavec-a8c6ac9d853d3569.d: src\lib.rs src\core.rs src\error.rs src\result.rs
+
+src\lib.rs:
+src\core.rs:
+src\error.rs:
+src\result.rs:
